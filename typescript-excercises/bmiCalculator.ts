bf9c580d915612e6f85b3177ad4c8ec46a8bb593
Juboskar <oskari.juvakka@gmail.com>
2022-06-20 15:36:33 +0300

    9.1

---
+++ typescript-excercises/bmiCalculator.ts
@@ -0,0 +1,12 @@
+const calculateBmi = (height: number, weight: number) => {
+  const bmi = weight / (height / 100) ** 2;
+  if (bmi < 18.5) {
+    return 'Underweight';
+  } else if (bmi > 25) {
+    return 'Overweight';
+  } else {
+    return 'Normal (healthy weight)';
+  }
+};
+
+console.log(calculateBmi(180, 74));
