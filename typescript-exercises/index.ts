570adf6e3734ab8cb836d103b12d024367b817df
Juboskar <oskari.juvakka@gmail.com>
2022-06-20 18:17:54 +0300

    9.5

--- typescript-exercises/index.ts
+++ typescript-exercises/index.ts
@@ -1,8 +1,21 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
+  res.status(200).send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.heigth);
+  const weight = Number(req.query.weight);
+  if (!height || !weight) {
+    res.status(400).json({
+      error: 'malformatted parameters',
+    });
+  }
+  const result = calculateBmi(height, weight);
+  res.status(200).json({ height, weight, result });
 });
 
 const PORT = 3003;
