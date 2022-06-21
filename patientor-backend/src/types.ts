7e96fd5f04ba5718cbd83f457217d69e279ed3c1
Juboskar <oskari.juvakka@gmail.com>
2022-06-21 04:04:00 +0300

    9.13

--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -9,8 +9,14 @@ export interface PatientEntry {
   name: string;
   dateOfBirth: string;
   ssn: string;
-  gender: string;
+  gender: Gender;
   occupation: string;
+}
+
+export enum Gender {
+  male = 'male',
+  female = 'female',
+  other = 'other',
 }
 
 export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn'>;
--- patientor-backend/src/utils.ts
+++ patientor-backend/src/utils.ts
@@ -1,5 +1,16 @@
 import { v1 as uuid } from 'uuid';
-import { PatientEntry } from './types';
+import { Gender, PatientEntry } from './types';
+
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date));
+};
+
+const parseDate = (date: unknown): string => {
+  if (!date || !isString(date) || !isDate(date)) {
+    throw new Error('Incorrect or missing date: ' + date);
+  }
+  return date;
+};
 
 const isString = (text: unknown): text is string => {
   return typeof text === 'string' || text instanceof String;
@@ -7,9 +18,22 @@ const isString = (text: unknown): text is string => {
 
 const parseStringValue = (text: unknown): string => {
   if (!text || !isString(text)) {
-    throw new Error('Incorrect or missing info');
+    throw new Error('Incorrect or missing name, ssn or occupation:' + text);
   }
   return text;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isGender = (param: any): param is Gender => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  return Object.values(Gender).includes(param);
+};
+
+const parseGender = (gender: unknown): Gender => {
+  if (!gender || !isGender(gender)) {
+    throw new Error('Incorrect or missing gender: ' + gender);
+  }
+  return gender;
 };
 
 type Fields = {
@@ -31,9 +55,9 @@ const toNewPatientEntry = ({
   const newEntry: PatientEntry = {
     id: id,
     name: parseStringValue(name),
-    dateOfBirth: parseStringValue(dateOfBirth),
+    dateOfBirth: parseDate(dateOfBirth),
     ssn: parseStringValue(ssn),
-    gender: parseStringValue(gender),
+    gender: parseGender(gender),
     occupation: parseStringValue(occupation),
   };
 
