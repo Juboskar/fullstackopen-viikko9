07d989b195c28a77e64df059cf157a3c8593bba2
Juboskar <oskari.juvakka@gmail.com>
2022-06-26 15:26:25 +0300

    fix to patient data typing

--- patientor-backend/src/services/patientService.ts
+++ patientor-backend/src/services/patientService.ts
@@ -2,7 +2,7 @@ import patientData from '../../data/patients.json';
 
 import { NonSensitivePatientEntry, PatientEntry } from '../types';
 
-const patients: Array<NonSensitivePatientEntry> = patientData;
+const patients: Array<NonSensitivePatientEntry> = patientData as PatientEntry[];
 
 const getEntries = (): NonSensitivePatientEntry[] => {
   return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
